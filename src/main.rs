5a0d7922a8fa89524f45cfef544a440a45ebb0ed
Alligator-52 <ayushankitpatnaik99@gmail.com>
2024-09-08 23:10:35 +0530

    implemented env variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 use std::fs;
@@ -5,7 +6,22 @@ use std::cmp::Reverse;
 use colored::*;
 
 const MAX_ENTRIES: usize = 10;
-fn main() 
+fn main() -> io::Result<()>
+{
+    
+    let args:Vec<String> = env::args().collect();
+    if args.len() < 2
+    {
+        return Ok(());
+    }
+    let path = Path::new(&args[1]);
+    let mut dir_paths: Vec<PathBuf> = Vec::new();
+    implement_search(path, &mut dir_paths);
+    return Ok(());
+}
+
+#[allow(dead_code)]
+fn user_input_method()
 {
     let mut dir_paths: Vec<PathBuf> = Vec::new();
     loop
@@ -13,32 +29,38 @@ fn main()
         let user_input = get_user_input();
         let path = Path::new(&user_input);
 
-        if !path.exists() || !path.is_dir()
-        {
-            println!("{}","\nprovided path is not a directory".red());
-            continue;
-        }
-        search_longest_paths_dfs(path, &mut dir_paths);
-        let len = match dir_paths.len()
-        {
-            len if len >= MAX_ENTRIES => MAX_ENTRIES,
-            len => len
-        };
-        dir_paths.sort_by_key(|p|Reverse(p.to_str().unwrap_or("").len()));
-
-        println!
-        (
-            "\nSuccessfuilly scanned: {}\nTotal entries found: {}\n{} longest paths:\n",
-            path.to_string_lossy().yellow(),
-            dir_paths.len().to_string().yellow(),
-            len.to_string().yellow()
-        );
-
-        for path in dir_paths.iter().take(MAX_ENTRIES)
-        {
-            println!("{}", path.to_string_lossy().green());
-        }
+        implement_search(path, &mut dir_paths);
+        
         break;
+    }
+}
+
+fn implement_search(path: &Path, dir_paths: &mut Vec<PathBuf>)
+{
+    if !path.exists() || !path.is_dir()
+    {
+        println!("{}","\nprovided path is not a directory".red());
+        return;
+    }
+    search_longest_paths_dfs(path, dir_paths);
+    let len = match dir_paths.len()
+    {
+        len if len >= MAX_ENTRIES => MAX_ENTRIES,
+        len => len
+    };
+    dir_paths.sort_by_key(|p|Reverse(p.to_str().unwrap_or("").len()));
+
+    println!
+    (
+        "\nSuccessfuilly scanned: {}\nTotal entries found: {}\n{} longest paths:\n",
+        path.to_string_lossy().yellow(),
+        dir_paths.len().to_string().yellow(),
+        len.to_string().yellow()
+    );
+
+    for path in dir_paths.iter().take(MAX_ENTRIES)
+    {
+        println!("{}", path.to_string_lossy().green());
     }
 }
 
